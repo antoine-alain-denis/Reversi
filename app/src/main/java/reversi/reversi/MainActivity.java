f53dffbcb86ac0712bfb13bd2c41ff96cb83c81d
antoine-alain-denis <antoine.alain.denis@gmail.com>
2016-07-28 12:11:39 +0200

    Corrected a bug

--- app/src/main/java/reversi/reversi/MainActivity.java
+++ app/src/main/java/reversi/reversi/MainActivity.java
@@ -29,56 +29,12 @@ public class MainActivity extends AppCompatActivity {
     private EditText et_new_strings;
     private ArrayList<String> al_strings;
     private ArrayAdapter<String> aa_strings;
-    private ArrayList<CustomItem> al_items;
-    private CustomArrayAdapter caa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
-
-        /*tv_display = (TextView) findViewById(R.id.tv_display);
-        lv_mainlist = (ListView) findViewById(R.id.lv_mainlist);
-        et_new_strings = (EditText) findViewById(R.id.et_new_strings);
-        al_items = new ArrayList<CustomItem>();
-        caa = new CustomArrayAdapter(this, al_items);
-        lv_mainlist.setAdapter(caa);
-
-        et_new_strings.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            public boolean onEditorAction(TextView view, int actionid, KeyEvent event)
-            {
-                if(actionid == EditorInfo.IME_ACTION_DONE) {
-                    al_items.add(new
-                            CustomItem(et_new_strings.getText().toString(), System.currentTimeMillis()));
-                    caa.notifyDataSetChanged();
-                    tv_display.setText("added item: " + et_new_strings.getText());
-                    return true;
-                }
-                return false;
-            }
-        });
-
-
-        lv_mainlist.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> adapterview, View view, int pos,
-                                    long id) {
-
-                tv_display.setText("item " + pos + " selected");
-            }
-        });
-
-        lv_mainlist.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-            public boolean onItemLongClick(AdapterView<?> adapterview, View view, int
-                    pos, long id) {
-
-                tv_display.setText("item " + pos + " long clicked");
-                return true;
-            }
-        });*/
 
     }
 
